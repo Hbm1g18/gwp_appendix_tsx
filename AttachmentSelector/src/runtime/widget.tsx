494aeb1aa848a920f9e1a7ff5449f25b546b3408
Harry Bonshor-Mayes <HarryBM@gwp.uk.com>
2024-05-09 15:45:22 +0100

    Testing github action for EC2 deployment

--- AttachmentSelector/src/runtime/widget.tsx
+++ AttachmentSelector/src/runtime/widget.tsx
@@ -378,7 +378,7 @@ export default function Widget(props: AllWidgetProps<{}>) {
             e.target.style.transform = 'scale(1)';
           }}
         >
-          Select Attachments
+          Testing action
         </button>
       )}
     </div>
